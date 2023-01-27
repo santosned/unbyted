9c72686ff0546888dc6beaeebf27761486f27b7d
E. M. Santos <santosned@proton.me>
2023-01-27 12:28:18 -0300

    types: update library type definitions

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -4,6 +4,9 @@ declare type UnitsLength<T, N extends number, R extends T[] = []> = number exten
   : R['length'] extends N
   ? R
   : UnitsLength<T, N, [T, ...R]>
+declare type Required<T> = {
+  [P in keyof T]-?: T[P]
+}
 declare interface UnbytedOptions {
   /** Include units of measurement or not. (default: true) */
   unit?: boolean
@@ -16,51 +19,53 @@ declare interface UnbytedOptions {
   /** Replace the default binary symbols with 7 other units symbols. */
   binaryUnits?: UnitsLength<string, 7>
   /** Display bytes (B) unit or not. (default: false) */
-  bytes?: boolean
+  includeBytes?: boolean
+  /** Change the default binary unit value. (default: 1024) */
+  defaultBinaryUnitValue?: number
+  /** Change the default decimal unit value. (default: 1000) */
+  defaultDecimalUnitValue?: number
 }
-declare class Unbyted {
-  private readonly applyUnit
-  private readonly allowBytes
-  private readonly trimDecimals
-  private readonly decimals
-  private readonly binaryUnitValue
-  private readonly decimalUnitValue
-  private readonly binaryUnits
-  private readonly decimalUnits
-  constructor(options?: UnbytedOptions)
+declare type UnbytedReturns = {
   /**
    * Formats bytes into readable `binary` measurement units.
    * @param {number} bytes the bytes to be formatted
    * @return {string} an string containing the formatted bytes
    * @since v0.1.0
    */
-  public toBinaryString(bytes: number): string
+  toBinaryString(bytes: number): string
   /**
    * Formats bytes into readable `decimal` measurement units.
    * @param {number} bytes the bytes to be formatted
    * @return {string} an string containing the formatted bytes
    * @since v0.1.0
    */
-  public toDecimalString(bytes: number): string
-  /**
-   * Automatically format bytes into readable measurement units. This is used internally only.
-   * @param {number} bytes the bytes to be formatted
-   * @param {number} unitValue the base unit value for the conversion
-   * @return {string} an string containing the formatted bytes
-   * @since v0.1.0
-   */
-  private autoFormat(bytes: number, unitValue: number): string
+  toDecimalString(bytes: number): string
 }
+
+/**
+ * Automatically format bytes into readable measurement units. This is used internally only.
+ * @param {number} bytes the bytes to be formatted
+ * @param {string} key the key of the selected unit value
+ * @return {string} an string containing the formatted bytes
+ * @since v0.1.0
+ */
+declare function autoFormat(
+  bytes: number,
+  key: 'binaryUnits' | 'decimalUnits',
+  config: Required<UnbytedOptions>
+): string
+
 /**
  * Creates new Unbyted instace to format bytes into readable measurement units.
  * @param options the options to control how the bytes will be formatted
- * @return an API with methods to format bytes
- * @since v0.4.0
+ * @return properties to format bytes into [decimal](https://github.com/santosned/unbyted#decimal) or [binary](https://github.com/santosned/unbyted#binary) measurement units.
+ * @since v1.0.0
  * @example
  * ```js
  * unbyted().toBinaryString(1000) // Returns: 0.98 KiB
  * unbyted().toDecimalString(1000) // Returns: 1.00 KB
  * ```
  */
-declare function unbyted(options?: UnbytedOptions): Unbyted
+declare function unbyted(options?: UnbytedOptions): UnbytedReturns
+
 export = unbyted
