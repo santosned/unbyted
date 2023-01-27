e874de370e5e2d32fc2db4689fdba3ac10fdbe5f
E. M. Santos <santosned@proton.me>
2023-01-27 12:26:28 -0300

    refactor!: turn Unbyted class into a function
    - fix: error messages typos
    - feat: add support for custom default unit values

--- package/types.ts
+++ package/types.ts
@@ -3,7 +3,9 @@ type UnitsLength<T, N extends number, R extends T[] = []> = number extends N
   : R['length'] extends N
   ? R
   : UnitsLength<T, N, [T, ...R]>
-
+export type Required<T> = {
+  [P in keyof T]-?: T[P]
+}
 export interface UnbytedOptions {
   /** Include units of measurement or not. (default: true) */
   unit?: boolean
@@ -21,5 +23,29 @@ export interface UnbytedOptions {
   binaryUnits?: UnitsLength<string, 7>
 
   /** Display bytes (B) unit or not. (default: false) */
-  bytes?: boolean
+  includeBytes?: boolean
+
+  /** Change the default binary unit value. (default: 1024) */
+  defaultBinaryUnitValue?: number
+
+  /** Change the default decimal unit value. (default: 1000) */
+  defaultDecimalUnitValue?: number
+}
+
+export type UnbytedReturns = {
+  /**
+   * Formats bytes into readable `binary` measurement units.
+   * @param {number} bytes the bytes to be formatted
+   * @return {string} an string containing the formatted bytes
+   * @since v0.1.0
+   */
+  toBinaryString(bytes: number): string
+
+  /**
+   * Formats bytes into readable `decimal` measurement units.
+   * @param {number} bytes the bytes to be formatted
+   * @return {string} an string containing the formatted bytes
+   * @since v0.1.0
+   */
+  toDecimalString(bytes: number): string
 }
