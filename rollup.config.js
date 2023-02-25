d20edc45ffec6fcf4e5f4305c5bc04acf2d886dc
E. M. Santos <santosned@proton.me>
2023-02-25 02:12:34 -0300

    chore!: replace babel with SWC
    chore: add benny for benchmark tests
    chore: add rollup-plugin-dts
    chore: update tsconfig.json
    chore: update rollup.config.js
    chore: update jest.config.js
    chore: update .npmignore

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,34 @@
-import terser from '@rollup/plugin-terser'
-import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts';
+import { swc, minify } from 'rollup-plugin-swc3';
 
-export default {
-  input: 'package/index.ts',
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'cjs',
-    },
-    {
-      file: 'dist/index.mjs',
-      format: 'esm',
-    },
-  ],
-  plugins: [
-    typescript({ compilerOptions: { declaration: false, emitDeclarationOnly: false } }),
-    terser({
-      ecma: 5,
-      module: true,
-      toplevel: true,
-      compress: { pure_getters: true },
-      format: { quote_style: 'original' },
-    }),
-  ],
-}
+export default [
+  {
+    input: 'package/index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/index.mjs',
+        format: 'esm',
+      },
+    ],
+    plugins: [
+      swc({
+        include: /\.[t]s?$/,
+        jsc: {},
+      }),
+      minify({
+        compress: true,
+        toplevel: true,
+        mangle: true,
+      }),
+    ],
+  },
+  {
+    input: 'package/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts.default({})],
+  },
+];
